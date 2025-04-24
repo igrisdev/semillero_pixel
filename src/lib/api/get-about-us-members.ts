8aca37cbee85f913e9e18ae5d3e467226b0a3025
igrisdev <johanalvarezz237@gmail.com>
2025-04-24 13:56:21 -0500

    Add getAboutUsMembers function to fetch member data from Strapi API

---
+++ src/lib/api/get-about-us-members.ts
@@ -0,0 +1,32 @@
+import { query } from './strapi'
+
+const { STRAPI_HOST } = import.meta.env
+
+export function getAboutUsMembers() {
+  return query(
+    'members?populate[0]=image_member&populate[1]=red_social_member.logo_red_social'
+  ).then((res) => {
+    const members = res.data.map((member: any) => {
+      const imageMember = `${STRAPI_HOST}${member.image_member.url}`
+      const imageSocial = member.red_social_member.map((social: any) => ({
+        logo_red_social: `${STRAPI_HOST}${social.logo_red_social.url}`,
+        name_social_media: social.name_red_social_media,
+        link_red_social_media: social.link_red_social_media,
+      }))
+
+      return {
+        image: imageMember,
+        name: member.name_member,
+        orcid: member.orcid_member,
+        rol: member.rol_member,
+        career: member.career_member,
+        state: member.state_member,
+        date_init: member.date_init_member,
+        date_end: member.date_end_member,
+        social_media: imageSocial,
+      }
+    })
+
+    return members
+  })
+}
