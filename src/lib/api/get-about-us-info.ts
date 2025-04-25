30caf7f67e3fb851ed531fbbd393c0d2aad1e8f4
igrisdev <johanalvarezz237@gmail.com>
2025-04-25 15:12:53 -0500

    Update getAboutUsInfo function to replace 'text_about_us' with 'description_about_us' for consistency; adjust AboutUs.astro to reflect changes

--- src/lib/api/get-about-us-info.ts
+++ src/lib/api/get-about-us-info.ts
@@ -2,7 +2,7 @@ import { query } from './strapi'
 
 export function getAboutUsInfo() {
   return query('about-us').then((res) => {
-    const { title_about_us, text_about_us } = res.data
-    return { title_about_us, text_about_us }
+    const { title_about_us, description_about_us } = res.data
+    return { title_about_us, description_about_us }
   })
 }
