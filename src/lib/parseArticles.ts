74584020bd773bddcec43b5d3ac3ac2623df9bb7
igrisdev <johanalvarezz237@gmail.com>
2025-04-26 14:02:13 -0500

    Refactor Articles.astro to fetch articles from API and parse them by year; remove hardcoded articles data

---
+++ src/lib/parseArticles.ts
@@ -0,0 +1,46 @@
+export function parseArticles(articles: any[]) {
+  const articlesByYear: any = {}
+
+  articles.forEach((article) => {
+    const year = article.date.split('-')[0]
+
+    if (!articlesByYear[year]) {
+      articlesByYear[year] = []
+    }
+
+    const dateObj = new Date(article.date)
+    const months = [
+      'enero',
+      'febrero',
+      'marzo',
+      'abril',
+      'mayo',
+      'junio',
+      'julio',
+      'agosto',
+      'septiembre',
+      'octubre',
+      'noviembre',
+      'diciembre',
+    ]
+    const formattedDate = `${dateObj.getDate()} ${months[dateObj.getMonth()]} ${year}`
+
+    articlesByYear[year].push({
+      title: article.title,
+      author: article.author,
+      date: formattedDate,
+      description: article.description,
+      link: article.link,
+      image_author: article.image_author,
+    })
+  })
+
+  const result = Object.keys(articlesByYear)
+    .sort((a: any, b: any) => b - a)
+    .map((year) => ({
+      year,
+      articles: articlesByYear[year],
+    }))
+
+  return result
+}
