eb2ccdb08e75a61dd0534a57c1c47dc44565b00e
igrisdev <johanalvarezz237@gmail.com>
2025-04-29 13:29:01 -0500

    Comment out Description component in index.astro

--- src/lib/api/get-home-hero.ts
+++ src/lib/api/get-home-hero.ts
@@ -4,8 +4,10 @@ const { STRAPI_HOST } = import.meta.env
 
 export function getHomeHero() {
   return query('home?populate=image_bg_hero').then((res) => {
+    console.log(res.data)
     const { title_hero, description_hero, image_bg_hero } = res.data
     const image_hero = `${STRAPI_HOST}${image_bg_hero.url}`
+
     return { title_hero, description_hero, image_hero }
   })
 }
