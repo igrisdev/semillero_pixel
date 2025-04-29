25a7cf7ed94ca6aaefb34b034e228941a9fdf020
igrisdev <johanalvarezz237@gmail.com>
2025-04-29 13:35:10 -0500

    chore: add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,6 @@ export default defineConfig({
       svg: true,
     },
   },
+
+  adapter: vercel(),
 })
