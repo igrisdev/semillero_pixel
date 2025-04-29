a03c6ec87d7c59df9c4c3de67e66a9b93afe9800
igrisdev <johanalvarezz237@gmail.com>
2025-04-29 13:40:33 -0500

    refactor: remove redundant logging from getAboutUsInfo function

--- src/lib/api/get-about-us-info.ts
+++ src/lib/api/get-about-us-info.ts
@@ -2,10 +2,6 @@ import { query } from './strapi'
 
 export function getAboutUsInfo() {
   return query('about-us').then((res) => {
-    console.log('----------------------------------------------------')
-    console.log(res.data)
-    console.log('----------------------------------------------------')
-    console.log('----------------------------------------------------')
     const { title_about_us, description_about_us } = res.data
     return { title_about_us, description_about_us }
   })
