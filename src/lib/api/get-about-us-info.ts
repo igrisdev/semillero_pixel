27cee6c783f51651039ad0cb06258a4982953fc1
igrisdev <johanalvarezz237@gmail.com>
2025-04-29 13:37:28 -0500

    feat: add logging for About Us data retrieval

--- src/lib/api/get-about-us-info.ts
+++ src/lib/api/get-about-us-info.ts
@@ -2,6 +2,9 @@ import { query } from './strapi'
 
 export function getAboutUsInfo() {
   return query('about-us').then((res) => {
+    console.log('----------------------------------------------------')
+    console.log(res.data)
+    console.log('----------------------------------------------------')
     const { title_about_us, description_about_us } = res.data
     return { title_about_us, description_about_us }
   })
