a66193866e7c97826ea87d8f8863a7a8ea4ddde6
igrisdev <johanalvarezz237@gmail.com>
2025-04-29 13:41:01 -0500

    refactor: remove console log from getHomeHero function and uncomment Description component in index.astro

--- src/lib/api/get-home-hero.ts
+++ src/lib/api/get-home-hero.ts
@@ -4,7 +4,6 @@ const { STRAPI_HOST } = import.meta.env
 
 export function getHomeHero() {
   return query('home?populate=image_bg_hero').then((res) => {
-    console.log(res.data)
     const { title_hero, description_hero, image_bg_hero } = res.data
     const image_hero = `${STRAPI_HOST}${image_bg_hero.url}`
 
