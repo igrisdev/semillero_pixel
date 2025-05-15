031dd8935c3e54b7147586ed4064480911f2f8d2
igrisdev <johanalvarezz237@gmail.com>
2025-05-15 16:38:37 -0500

    feat: add parseProjects function to organize projects by deployment year

---
+++ src/lib/parseProjects.ts
@@ -0,0 +1,24 @@
+import { parseDate } from './parseDate'
+
+export function parseProjects(projects: any[]) {
+  const projectsByYear: any = {}
+
+  projects.forEach((project) => {
+    const year = project.date_deploy.split('-')[0]
+
+    if (!projectsByYear[year]) {
+      projectsByYear[year] = []
+    }
+
+    projectsByYear[year].push(project)
+  })
+
+  const result = Object.keys(projectsByYear)
+    .sort((a: any, b: any) => b - a)
+    .map((year) => ({
+      year,
+      projects: projectsByYear[year],
+    }))
+
+  return result
+}
