20cd1d8057f9803c56c5b7689de841bca5ed0b86
igrisdev <johanalvarezz237@gmail.com>
2025-06-13 20:37:36 -0500

    fix: refactor Event interface to correct structure and update events array

--- src/mockups/events.mockup.ts
+++ src/mockups/events.mockup.ts
@@ -2,24 +2,26 @@ import Image1 from '@assets/images/events/v-encuentro-internacional-de-semillero
 import Image2 from '@assets/images/events/v-encuentro-internacional-de-semilleros-de-investigación-prexia-2024/2.jpg'
 
 export interface Event {
-  picture_event: {
+  events: {
     images_event: string[]
     date_init_event: string
     date_end_event: string
     title_event: string
-  }
+  }[]
   publication: string
 }
 
 export const EVENTS: Event[] = [
   {
-    picture_event: {
-      images_event: [Image1.src, Image2.src],
-      date_init_event: '2024-10-24',
-      date_end_event: '2024-10-25',
-      title_event:
-        'V Encuentro internacional de semilleros de investigación Prexia 2024',
-    },
+    events: [
+      {
+        images_event: [Image1.src, Image2.src],
+        date_init_event: '2024-10-24',
+        date_end_event: '2024-10-25',
+        title_event:
+          'V Encuentro internacional de semilleros de investigación Prexia 2024',
+      },
+    ],
     publication:
       'Arquitectura de la información aplicada en el diseño de un repositorio de recursos educativos de estadística',
   },
