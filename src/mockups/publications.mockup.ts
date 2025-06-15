69cdc912d6535142f20dc945a75ea8b7c1954c38
igrisdev <johanalvarezz237@gmail.com>
2025-06-15 12:21:03 -0500

    Refactor code structure for improved readability and maintainability

--- src/mockups/publications.mockup.ts
+++ src/mockups/publications.mockup.ts
@@ -1,3 +1,5 @@
+import PDFArquitecturaDeLaInformacionAplicadaEnElDisenoDeUnRepositorioDeRecursosEducativosDeEstatistica from '@assets/pdf/publications/Arquitectura de la información aplicada en el diseño de un repositorio de recursos educativos de estadística.pdf'
+
 export interface Publication {
   title: string
   publisher: string
@@ -24,11 +26,11 @@ export const PUBLICATIONS: Publication[] = [
     ],
     date_publication: '2024-10-24',
     description:
-      'Al realizar búsquedas sobre estadística, a menudo se presenta el inconveniente de no encontrar material confiable, ya sea para reforzar los conocimientos de los alumnos o el desarrollo de proyectos en los que se requiere el uso de esta área.',
+      'En la educación superior, el acceso y la organización eficiente de material educativo es crucial para un aprendizaje efectivo, especialmente en la estadística. Los recursos educativos disponibles en línea a menudo son variados y no siempre se ajustan a las necesidades específicas de los estudiantes, lo que dificulta su aprendizaje. Por ello se propone desarrollar un repositorio que agrupe estos recursos de manera coherente y optimice su accesibilidad y usabilidad. Se implementarán principios de arquitectura de la información y se priorizan las necesidades del usuario, esto incluye, una interfaz intuitiva y mecanismos de búsqueda eficientes. Este enfoque proporciona una herramienta adecuada para el aprendizaje de la estadística. La evaluación del sistema se realizará desde la experiencia de usuario, con la participación de estudiantes, docentes y expertos en usabilidad, verificando la relevancia y efectividad del repositorio.',
     slug: 'arquitectura-de-la-información-aplicada-en-el-diseño-de-un-repositorio-de-recursos educativos-de-estadística',
-    write_all_the_information:
-      'Al realizar búsquedas sobre estadística, a menudo se presenta el inconveniente de no encontrar material confiable, ya sea para reforzar los conocimientos de los alumnos o el desarrollo de proyectos en los que se requiere el uso de esta área. ',
-    download_file: 'https://images6.alphacoders.com/105/thumb-1920-1050408.jpg',
+    write_all_the_information: 'Al',
+    download_file:
+      PDFArquitecturaDeLaInformacionAplicadaEnElDisenoDeUnRepositorioDeRecursosEducativosDeEstatistica,
     type_publications: ['Articulo Largo'],
   },
 ]
