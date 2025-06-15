afca15ded53ff0c59addd7c1139f69017e855556
igrisdev <johanalvarezz237@gmail.com>
2025-06-15 13:36:17 -0500

    Implement code changes to enhance functionality and improve performance

--- src/mockups/data.ts
+++ src/mockups/data.ts
@@ -5,7 +5,10 @@ export const hero: Hero[] = [
     title_hero: 'Descripción',
     description_hero:
       'Somos un semillero enfocado en la investigación y el desarrollo de proyectos web. Deseamos impactar el mundo de la web y crear soluciones innovadoras para mejorar la experiencia de los usuarios.',
-    image_hero: 'https://images6.alphacoders.com/105/thumb-1920-1050408.jpg',
+    // image_hero: 'https://images6.alphacoders.com/105/thumb-1920-1050408.jpg',
+    // image_hero:
+    //   'https://img.freepik.com/vector-premium/ilustracion-arte-pixeles-campamento-fondo-campamento-pixelado-paisaje-campamento-pixelado-juego_1038602-1444.jpg',
+    image_hero: '/public/icons/bg_default_freepik.jpg',
   },
 ]
 
