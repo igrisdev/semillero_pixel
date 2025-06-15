9f0a96773b363a0d6153021e5edd89b78982697e
igrisdev <johanalvarezz237@gmail.com>
2025-06-15 13:18:20 -0500

    chore: remove commented-out code for cleaner implementation

--- src/lib/api/get-one-event.ts
+++ src/lib/api/get-one-event.ts
@@ -24,35 +24,3 @@ export async function getOneEvent(slug: string): Promise<EventOne> {
     slug_event: findEvent[0]!.slug_event,
   })
 }
-
-// import { query } from './strapi'
-
-// const { STRAPI_HOST_IMG } = import.meta.env
-
-// export function getHomeEventsCard() {
-//   return query(
-//     'events?populate=picture_event.images_event&populate=picture_event.publication'
-//   ).then((res) => {
-//     const events = res.data.map((event: any) => {
-//       const newEvent = event.picture_event.map((info: any) => {
-//         const image_link = `${STRAPI_HOST_IMG}${info.images_event[0].url}`
-//         const publication = info.publication.slug
-
-//         return {
-//           image: image_link,
-//           slug: publication,
-//         }
-//       })
-
-//       const year = new Date(event.date_start).getFullYear()
-
-//       return {
-//         year,
-//         title: event.title_event,
-//         infoEvent: newEvent,
-//       }
-//     })
-
-//     return events
-//   })
-// }
