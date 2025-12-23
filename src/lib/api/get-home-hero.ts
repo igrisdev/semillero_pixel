b484ba879d1add59da88f029294acb33a4e4946e
igrisdev <johanalvarezz237@gmail.com>
2025-12-23 17:38:24 -0500

    Refactor code structure for improved readability and maintainability

--- src/lib/api/get-home-hero.ts
+++ src/lib/api/get-home-hero.ts
@@ -1,19 +1,19 @@
-import { hero } from 'src/mockups/data'
-import type { Hero } from 'src/types/data.types'
+// import { hero } from 'src/mockups/data'
+// import type { Hero } from 'src/types/data.types'
 
-export async function getHomeHero(): Promise<Hero> {
-  return await Promise.resolve(hero[0])
-}
-
-// import { query } from './strapi'
-
-// const { STRAPI_HOST_IMG } = import.meta.env
-
-// export function getHomeHero() {
-//   return query('home?populate=image_bg_hero').then((res) => {
-//     const { title_hero, description_hero, image_bg_hero } = res.data
-//     const image_hero = `${STRAPI_HOST_IMG}${image_bg_hero.url}`
-
-//     return { title_hero, description_hero, image_hero }
-//   })
+// export async function getHomeHero(): Promise<Hero> {
+//   return await Promise.resolve(hero[0])
 // }
+
+import { query } from './strapi'
+
+const { STRAPI_HOST_IMG } = import.meta.env
+
+export function getHomeHero() {
+  return query('home?populate=image_bg_hero').then((res) => {
+    const { title_hero, description_hero, image_bg_hero } = res.data
+    const image_hero = `${STRAPI_HOST_IMG}${image_bg_hero.url}`
+
+    return { title_hero, description_hero, image_hero }
+  })
+}
--- src/lib/api/get-home-projects.ts
+++ src/lib/api/get-home-projects.ts
@@ -1,83 +1,83 @@
-import { PROJECTS } from 'src/mockups/projects.mockup'
-import { PUBLICATIONS, type Publication } from 'src/mockups/publications.mockup'
-import type { ProjectCard } from 'src/types/data.types'
+// import { PROJECTS } from 'src/mockups/projects.mockup'
+// import { PUBLICATIONS, type Publication } from 'src/mockups/publications.mockup'
+// import type { ProjectCard } from 'src/types/data.types'
 
-export async function getHomeProjects(): Promise<ProjectCard[]> {
-  const newProjects = PROJECTS.map((project: any) => {
-    const technologies = project.technology_project.map((tech: any) => {
-      return {
-        title: tech.title_technology_project,
-        link: tech.link_page_technology_project,
-      }
-    })
-
-    const developers = project.members.map((member: any) => {
-      return {
-        name: member,
-      }
-    })
-
-    const publication = PUBLICATIONS.find(
-      ({ title }: Publication) => title === project.publication
-    )
-
-    return {
-      title: project.title_project,
-      image: project.image_project,
-      link_github: project.link_github,
-      slug: publication!.slug,
-      id: project.slug,
-      description: project.description_project,
-      technologies: technologies,
-      content: project.article_content_project,
-      author: project.person_name_article_publisher_project,
-      developers: developers,
-      date_deploy: project.date_deploy_project,
-    }
-  })
-
-  return await Promise.resolve(newProjects)
-}
-
-// import { query } from './strapi'
-
-// const { STRAPI_HOST_IMG } = import.meta.env
-
-// export function getHomeProjects() {
-//   return query(
-//     'projects?populate=image_project&populate=technology_project&populate=members&populate=publication'
-//   ).then((res) => {
-//     const projects = res.data.map((project: any) => {
-//       const technologies = project.technology_project.map((tech: any) => {
-//         return {
-//           title: tech.title_technology_project,
-//           link: tech.link_page_technology_project,
-//         }
-//       })
-
-//       const developers = project.members.map((member: any) => {
-//         return {
-//           name: member.name_member,
-//         }
-//       })
-
-//       const image_project = `${STRAPI_HOST_IMG}${project.image_project?.url}`
-
+// export async function getHomeProjects(): Promise<ProjectCard[]> {
+//   const newProjects = PROJECTS.map((project: any) => {
+//     const technologies = project.technology_project.map((tech: any) => {
 //       return {
-//         title: project.title_project,
-//         image: image_project,
-//         link_github: project.link_github,
-//         slug: project?.publication?.slug,
-//         id: project.slug,
-//         description: project.description_project,
-//         technologies: technologies,
-//         content: project.article_content_project,
-//         author: project.person_name_article_publisher_project,
-//         developers: developers,
-//         date_deploy: project.date_deploy_project,
+//         title: tech.title_technology_project,
+//         link: tech.link_page_technology_project,
 //       }
 //     })
 
-//     return projects
+//     const developers = project.members.map((member: any) => {
+//       return {
+//         name: member,
+//       }
+//     })
+
+//     const publication = PUBLICATIONS.find(
+//       ({ title }: Publication) => title === project.publication
+//     )
+
+//     return {
+//       title: project.title_project,
+//       image: project.image_project,
+//       link_github: project.link_github,
+//       slug: publication!.slug,
+//       id: project.slug,
+//       description: project.description_project,
+//       technologies: technologies,
+//       content: project.article_content_project,
+//       author: project.person_name_article_publisher_project,
+//       developers: developers,
+//       date_deploy: project.date_deploy_project,
+//     }
 //   })
+
+//   return await Promise.resolve(newProjects)
 // }
+
+import { query } from './strapi'
+
+const { STRAPI_HOST_IMG } = import.meta.env
+
+export function getHomeProjects() {
+  return query(
+    'projects?populate=image_project&populate=technology_project&populate=members&populate=publication'
+  ).then((res) => {
+    const projects = res.data.map((project: any) => {
+      const technologies = project.technology_project.map((tech: any) => {
+        return {
+          title: tech.title_technology_project,
+          link: tech.link_page_technology_project,
+        }
+      })
+
+      const developers = project.members.map((member: any) => {
+        return {
+          name: member.name_member,
+        }
+      })
+
+      const image_project = `${STRAPI_HOST_IMG}${project.image_project?.url}`
+
+      return {
+        title: project.title_project,
+        image: image_project,
+        link_github: project.link_github,
+        slug: project?.publication?.slug,
+        id: project.slug,
+        description: project.description_project,
+        technologies: technologies,
+        content: project.article_content_project,
+        author: project.person_name_article_publisher_project,
+        developers: developers,
+        date_deploy: project.date_deploy_project,
+      }
+    })
+
+    return projects
+  })
+}
